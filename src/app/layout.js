339b5ff9cb6c5e572c84a57340aa1b65e6405227
Viraj Nalbalwar <virajnalbalwar@gmail.com>
2024-12-28 11:55:06 +0530

    adding analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -14,10 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${outfit.className} ${outfit.className} antialiased`}
-      >
+      <body className={`${outfit.className} ${outfit.className} antialiased`}>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
